b41b1dee8fd818af9f9b702c62cab5e55a01fb34
Ariel Chercasky <arielc@vsisoft.com>
2017-03-21 17:13:11 -0300

    add rx classes

---
+++ app/src/main/java/com/chachapps/baselibrary/rx/view/HomeMvpView.java
@@ -0,0 +1,10 @@
+package com.chachapps.baselibrary.rx.view;
+
+import com.chachapps.initialclasses.mvp.view.BaseMvpView;
+
+/**
+ * Created by arichercasky on 21/3/17.
+ */
+
+public interface HomeMvpView extends BaseMvpView {
+}
