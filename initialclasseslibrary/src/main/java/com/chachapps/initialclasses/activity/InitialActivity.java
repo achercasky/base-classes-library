53660531d359bad10cf533912aed4ead43d7e4e6
Ariel Chercasky <arielc@vsisoft.com>
2017-04-03 13:54:04 -0300

    add methods for the initial activity class

--- initialclasseslibrary/src/main/java/com/chachapps/initialclasses/activity/InitialActivity.java
+++ initialclasseslibrary/src/main/java/com/chachapps/initialclasses/activity/InitialActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
 
 import com.chachapps.initialclasses.R;
 
@@ -17,7 +18,21 @@ public abstract class InitialActivity extends AppCompatActivity{
     private String newFragment;
     private String currentFragment;
 
-    public abstract int getMyFragment();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(isSplashScreen()) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+
+        bindView();
+
+        injectClass();
+
+        initListeners();
+    }
 
     /**
      * This method loads the initial fragment
@@ -36,7 +51,34 @@ public abstract class InitialActivity extends AppCompatActivity{
             t.add(getMyFragment(), fragment, newFragment);
             t.commit();
         }
-
-
     }
+
+    /**
+     * This method loads the fragment with the parameter to define a transition
+     * @param fragment
+     * @param addToBackStack
+     * @param transition
+     */
+    protected void changeFragment(Fragment fragment, boolean addToBackStack, int transition) {
+
+        newFragment = fragment.getClass().getName();
+
+        if (currentFragment == null || fragment == null || currentFragment.compareTo(newFragment) != 0) {
+            FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+
+            t.setTransition(transition);
+            t.add(getMyFragment(), fragment, newFragment);
+            t.commit();
+        }
+    }
+
+    protected abstract int getMyFragment();
+
+    protected abstract boolean isSplashScreen();
+
+    protected abstract void bindView();
+
+    protected abstract void injectClass();
+
+    protected abstract void initListeners();
 }
--- initialclasseslibrary/src/main/java/com/chachapps/initialclasses/fragment/BaseFragment.java
+++ initialclasseslibrary/src/main/java/com/chachapps/initialclasses/fragment/BaseFragment.java
@@ -1,8 +1,12 @@
 package com.chachapps.initialclasses.fragment;
 
+import android.support.v4.app.Fragment;
+
 /**
  * Created by arichercasky on 21/3/17.
  */
 
-public class BaseFragment {
+public class BaseFragment extends Fragment {
+
+
 }
